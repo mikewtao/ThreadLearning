b5d8a59d1627cbcde42b5dcb07d98d30533168b8
scottwt <forwangtao@gmail.com>
2017-03-31 19:25:48 +0800

    初次提交

---
+++ src/main/java/com/mike/ThreadLearning/CallableTest.java
@@ -0,0 +1,32 @@
+package com.mike.ThreadLearning;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+/**
+ * 
+ * @author scott
+ * 
+ */
+public class CallableTest {
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static void main(String[] args) {
+		ExecutorService excutor=Executors.newSingleThreadExecutor();
+		Future future=excutor.submit(new Callable(){
+			public String call() throws Exception {
+				Thread.sleep(5000L);
+				return "hello thread!";
+			}
+		});
+		try{
+		 System.out.println("waiting for finish");
+		 System.out.println(future.get());
+		 excutor.shutdown();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+}
