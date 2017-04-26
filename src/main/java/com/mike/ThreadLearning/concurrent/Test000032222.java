029defbae6327cb47b7f046fc2a9fb873e1ea1b4
scottwt <forwangtao@gmail.com>
2017-04-26 21:29:06 +0800

    ssss

---
+++ src/main/java/com/mike/ThreadLearning/concurrent/Test000032222.java
@@ -0,0 +1,10 @@
+package com.mike.ThreadLearning.concurrent;
+
+public class Test000032222 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub  
+		 System.out.println("111111");
+	}
+
+}
